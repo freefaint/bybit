9445ccd6fa238af66c7beae50886f57432c3eaa1
Andrey Tereshchenko <404442@gmail.com>
2025-09-03 17:48:38 +0300

    fix

--- apps/frontend/src/lib/socket.ts
+++ apps/frontend/src/lib/socket.ts
@@ -1,5 +1,5 @@
 import { io } from 'socket.io-client';
-export const socket = io('http://localhost:3000', { transports: ['websocket'] });
+export const socket = io('/', { transports: ['websocket'] });
 
 export type StreamEvent =
   | { type: 'ticker'; payload: any }
